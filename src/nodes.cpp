7e36edff5b17f664c9e86b5876f6ee61b6d94750
João Lucas Lauritzen <lukemagike@gmail.com>
2025-07-19 01:29:50 -0300

    some bad initial code

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -5,7 +5,7 @@ Vs_Node::Vs_Node()
     
 }
 
-int Vs_Node::make_node(Node_Type *t, int node_id, int x_pos, int y_pos, std::vector<Connection>connections)
+int Vs_Node::make_node(Node_Type *t, int node_id, int x_pos, int y_pos, int* connection_size, std::vector<Connection>connections)
 {  
     connection.clear();
 
@@ -14,6 +14,49 @@ int Vs_Node::make_node(Node_Type *t, int node_id, int x_pos, int y_pos, std::vec
     node_x = x_pos;
     node_y = y_pos;
     
+    // Node node = get_node_type_struct(*t);
+    int count = node.connection_count;
+    for(int i = 0; i < count; i++) {
+        connection.push_back({});
+        connection[i].set_connection_id(*connection_size++);
+    }
+
+    switch(node_type) {
+        case BEGIN:
+            connection[0].set_type(Connection::EXECUTE_NODE_FORWARD);
+            connection[0].set_pos(Connection::TOP_RIGHT);
+            break;
+        case IF_STATEMENT:
+            connection[0].set_type(Connection::EXECUTE_NODE_BACK);
+            connection[0].set_pos(Connection::TOP_LEFT);
+            connection[1].set_type(Connection::EXECUTE_NODE_FORWARD);
+            connection[1].set_pos(Connection::TOP_RIGHT);
+            connection[2].set_type(Connection::BRANCH);
+            connection[2].set_pos(Connection::BOTTOM_RIGHT);
+            break;
+        case FOR_LOOP:
+            connection[0].set_type(Connection::EXECUTE_NODE_BACK);
+            connection[0].set_pos(Connection::TOP_LEFT);
+            connection[1].set_type(Connection::EXECUTE_NODE_FORWARD);
+            connection[1].set_pos(Connection::TOP_RIGHT);
+            connection[2].set_type(Connection::VARIABLE);
+            connection[2].set_pos(Connection::BOTTOM_LEFT);
+            connection[3].set_type(Connection::BRANCH);
+            connection[3].set_pos(Connection::BOTTOM_RIGHT);
+            connection[4].set_type(Connection::BREAK);
+            connection[4].set_pos(Connection::BOTTOM_RIGHT);
+            break;
+        case ASSIGNMENT:
+            connection[0].set_type(Connection::EXECUTE_NODE_BACK);
+            connection[0].set_pos(Connection::TOP_LEFT);
+            connection[1].set_type(Connection::EXECUTE_NODE_FORWARD);
+            connection[1].set_pos(Connection::TOP_RIGHT);
+            break;
+        default:
+            std::cout << "out of range node type" << std::endl; 
+            return -1;
+    }
+
     return 0;
 }
 
@@ -27,8 +70,11 @@ int Vs_Node::get_node_id()
     return id;
 }
 
-int Vs_Node::place_node()
+int Vs_Node::place_node(int x_pos, int y_pos)
 {
+    node_x = x_pos;
+    node_y = y_pos;
+
     return 0;
 }
 
